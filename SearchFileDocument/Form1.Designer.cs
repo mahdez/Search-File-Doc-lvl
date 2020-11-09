400e253a5944956662b671d8cb377f0e168a797d
M.mahdee <mahdee.dark@gmail.com>
2020-11-09 15:49:25 +0700

    cmt

---
+++ SearchFileDocument/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SearchFileDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.strPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(422, 32);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(35, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(503, 32);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.FileName,
+            this.strPath});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(741, 434);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Index
+            // 
+            this.Index.HeaderText = "No.";
+            this.Index.Name = "Index";
+            this.Index.ReadOnly = true;
+            this.Index.Width = 50;
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileName.HeaderText = "FileName";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            // 
+            // strPath
+            // 
+            this.strPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.strPath.HeaderText = "Path";
+            this.strPath.Name = "strPath";
+            this.strPath.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Path File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Search File Document";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn strPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SearchFileDocument/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchFileDocument
+{
+    public partial class Form1 : Form
+    {
+        public static int i = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Searching...";
+            ChooseFolder();
+
+        }
+
+        public void ChooseFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            ClearData();
+            var dir = textBox1.Text;
+            PrintDirectoryTree(dir, 99, null);
+        }
+
+        private void ClearData()
+        {
+            if (i > 1)
+            {
+                i = 1;
+            }
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+        }
+
+        public void PrintDirectoryTree(string directory, int lvl, string[] excludedFolders, string lvlSeperator = "" )
+        {
+            excludedFolders = excludedFolders ?? new string[0];
+            if (directory != null && directory != "")
+            {
+                foreach (string f in Directory.GetFiles(directory))
+                {
+                    this.dataGridView1.Rows.Add(i++, Path.GetFileName(f), directory);
+                }
+                foreach (string d in Directory.GetDirectories(directory))
+                {
+                    this.dataGridView1.Rows.Add(i++, Path.GetFileName(d), directory);
+
+                    if (lvl > 0 && Array.IndexOf(excludedFolders, Path.GetFileName(d)) < 0)
+                    {
+                        PrintDirectoryTree(d, lvl - 1, excludedFolders, lvlSeperator + "  ");
+                    }
+                }
+            }
+            else
+            {
+                string message = "Please Check Path File";
+                MessageBox.Show(message);
+            }
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+
